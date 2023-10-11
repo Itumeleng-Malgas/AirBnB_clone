94dfa7ab7585a03b0611f80b3c4947653106c606
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-10-11 04:57:36 +0200

    file_storage

---
+++ models/engine/file_storage.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" This ...."""
+
+import json
+
+class FileStorage:
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        return FileStorage.__objects
+
+    def new(self, obj):
+        key = f"{obj.__class__.____name__}.{obj.id}"
+        FileStorage.__objects[key] = obj
+
+    def save(self):
+        data = {}
+        for key, obj in FileStorage.__objects.items():
+            data[key] = obj.to_dict()
+        with open(FileStorage.__file_path, 'w') as file:
+            json.dump(data, file)
+
+    def reload(self):
+        try:
+            with open(FileStorage.__file_path, 'r') as file:
+                data = json.load(file)
+                for key, value in data.items():
+                    class_name, obj_id = key.split('.')
+                    obj = models[class_name](**value)
+                    FileStorage.__objects[key] = obj
+        except FileNotFoundError:
+            pass
