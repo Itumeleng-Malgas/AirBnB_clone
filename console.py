bc09f63f3426066a35c3db8f971d77c0b6b04d0b
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-10-13 13:38:49 +0200

    console.py

---
+++ console.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+This modules defines HBNBCommand class, it extends the built-in cmd
+module and uses its cmdloop to create the interactive command prompt
+
+"""
+
+# User defined modules
+import models
+
+# Built-in modules
+import cmd
+import sys
+
+class HBNBCommand(cmd.Cmd):
+    """ HBNBCommand class implementation, extends Cmd module """
+    prompt = "> "
+
+    def do_quit(self, args):
+        """Exit the application."""
+        return True
+
+    def do_EOF(self, args):
+        """Exit the program on EOF (Ctrl+D)"""
+        return True
+
+    def emptyline(self):
+        """Take no action"""
+        pass
+
+# Inintialize HBNBCommanf class
+console = HBNBCommand()
+
+if __name__ == "__main__":
+    if not sys.stdin.isatty():
+
+        # Read commands from stdin (pipe)
+        for line in sys.stdin:
+            console.onecmd(line.strip())
+
+    else:
+        # Start the interactive cmdloop
+        console.cmdloop()
